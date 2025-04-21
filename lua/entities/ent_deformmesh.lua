e5b30bb513b5e456cdc66e4b8df44661137eff7f
Ty4a925 <akkauntnuba56@gmail.com>
2025-04-21 19:03:12 +0500

    mega update 😎

--- lua/entities/ent_deformmesh.lua
+++ lua/entities/ent_deformmesh.lua
@@ -17,6 +17,7 @@ local SERVER = SERVER
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 
+ENT.Editable = true
 ENT.PrintName		= "Deformation"
 ENT.Author			= "Ty4a"
 ENT.Instructions	= "Do you have any ideas where I can use this?"
@@ -31,6 +32,28 @@ ENT.MyMESH = nil
 ENT.MAT = Material("models/shiny")
 
 local model = IsMounted("cstrike") and "models/props/de_nuke/car_nuke_red.mdl" or "models/props_interiors/Furniture_Couch01a.mdl"
+
+function ENT:SetupDataTables()
+    self:NetworkVar( "Float", 0, "RadiusCOEF", { KeyName = "radiuscoef", Edit = { type = "Float", min = 0.1, max = 1, order = 1, title = "Radius Coefficent", category = "Collide"} } )
+    --self:NetworkVar( "Float", 1, "DamageCOEF", { KeyName = "damagecoef", Edit = { type = "Float", min = 0.005, max = 10, order = 2, title = "Damage Coefficent", category = "Collide"} } )
+    -- LazyTown :D
+
+    self:NetworkVar( "Bool", 0, "SoundTOG", { KeyName = "soundarmatura", Edit = { type = "Bool", order = 3, title = "Toggle Sound", category = "Visual & Sound"} } )
+    self:NetworkVar( "Bool", 1, "EffectTOG", { KeyName = "effectarmatura", Edit = { type = "Bool", order = 4, title = "Toggle Effects", category = "Visual & Sound"} } )
+    self:NetworkVar( "Int", 0, "SoundTYPE", { KeyName = "soundarmtype", Edit = { type = "Int", min = 1, max = 6, order = 5, title = "Sound Type", category = "Visual & Sound"} } )
+
+    if SERVER then
+        self:NetworkVarNotify( "RadiusCOEF", self.SVOptRadiuscoef )
+        self:NetworkVarNotify( "SoundTOG", self.SVOptSoundtog )
+        self:NetworkVarNotify( "EffectTOG", self.SVOptEffecttog )
+        self:NetworkVarNotify( "SoundTYPE", self.SVOptSoundtype )
+
+        self:SetRadiusCOEF(0.2)
+        self:SetSoundTOG(true)
+        self:SetEffectTOG(true)
+        self:SetSoundTYPE(1)
+    end
+end
 
 function ENT:SpawnFunction( ply, tr, ClassName )
 	if ( !tr.Hit ) then return end
@@ -90,7 +113,7 @@ if SERVER then
             net.WriteVector(dmginfo:GetDamagePosition())
             net.WriteNormal(-dmginfo:GetDamageForce())
             local net_WriteFloat = net.WriteFloat
-            net_WriteFloat(math.min(dmg,15))
+            net_WriteFloat(math.min(dmg, 15))
             net_WriteFloat(dmg)
         net.Broadcast()
     end
@@ -99,29 +122,32 @@ if SERVER then
         if data.DeltaTime > 0.1 then
             local speed = data.Speed
             if speed >= 150 then
-                local ef = EffectData()
-                    ef:SetEntity(self)
-                    ef:SetOrigin(data.HitPos)
-                    ef:SetNormal(data.HitNormal)
-                util.Effect("cardamage_a", ef)
+
+                local hitpos = data.HitPos
+                
+                if self.SVEffectTog then
+                    local ef = EffectData()
+                        ef:SetEntity(self)
+                        ef:SetOrigin(hitpos)
+                        ef:SetNormal(data.HitNormal)
+                    util.Effect("cardamage_a", ef)
+                end
 
                 local math_random = math.random
 
-                local sound = "physics/metal/metal_canister_impact_soft" .. math_random(1, 3) .. ".wav"
-                self:EmitSound(sound, 75, math_random(75, 150), speed*0.1)
-
-                local impactForce = speed * phys:GetMass()
-
-                local velocity = self:GetVelocity() or vector_origin
-                local nv = velocity:GetNormalized()
+                if self.SVSoundTog then
+                    local sound = self.SVSoundType .. math_random(1, 3) .. ".wav"
+                    self:EmitSound(sound, 75, math_random(75, 150), speed * 0.1)
+                end
 
                 net.Start("Deformation_Apply")
                     net.WriteEntity(self)
-                    net.WriteVector(data.HitPos)
-                    net.WriteNormal(nv)
+                    net.WriteVector(hitpos)
+
                     local net_WriteFloat = net.WriteFloat
-                    net_WriteFloat(math.min(speed*0.005,10))
-                    net_WriteFloat(self.MYSIZEOBB*0.3)
+
+                    net_WriteFloat(math.min(speed * 0.005, 10))
+                    net_WriteFloat(self.MYSIZEOBB * self.SVRadiuscoef)
                 net.Broadcast()
             end
         end
@@ -137,15 +163,57 @@ if SERVER then
         end
     end
 
+    local sss = {
+        [1] = "physics/metal/metal_canister_impact_soft",
+        [2] = "physics/metal/metal_chainlink_impact_hard",
+        [3] = "physics/metal/metal_computer_impact_hard",
+        [4] = "physics/metal/metal_barrel_impact_hard",
+        [5] = "physics/metal/metal_sheet_impact_hard",
+        [6] = "physics/metal/metal_solid_impact_bullet"
+    }
+
+    function ENT:SVOptSoundtype(_, oldvalue, newvalue)
+        if ( oldvalue == newvalue ) then return end
+
+        self.SVSoundType = sss[newvalue]
+    end
+
+    function ENT:SVOptSoundtog(_, oldvalue, newvalue)
+        if ( oldvalue == newvalue ) then return end
+
+        self.SVSoundTog = newvalue
+    end
+
+    function ENT:SVOptEffecttog(_, oldvalue, newvalue)
+        if ( oldvalue == newvalue ) then return end
+
+        self.SVEffectTog = newvalue
+    end
+
+    function ENT:SVOptRadiuscoef(_, oldvalue, newvalue)
+        if ( oldvalue == newvalue ) then return end
+
+        self.SVRadiuscoef = newvalue
+    end
+
 end
 
 if SERVER then return end
 
+local meta = FindMetaTable("Entity")
+ENT.Draw = meta.DrawModel
+
+local metavec = FindMetaTable("Vector")
+local GetNormalized = metavec.GetNormalized
+local Cross = metavec.Cross
+local DistToSqr = metavec.DistToSqr
+
 local function generateUV(vertices, scale)
 
     local function calculateUV(vertex, normal)
-        vertex.u = vertex.pos.x * scale
-        vertex.v = vertex.pos.y * scale
+        local pos = vertex.pos
+        vertex.u = pos.x * scale
+        vertex.v = pos.y * scale
     end
 
     for i = 1, #vertices - 2, 2 do
@@ -153,7 +221,7 @@ local function generateUV(vertices, scale)
 
         local apos = a.pos
 
-        local normal = (b.pos - apos):Cross(c.pos - apos):GetNormalized()
+        local normal = GetNormalized(Cross(b.pos - apos, c.pos - apos))
 
         calculateUV(a, normal)
         calculateUV(b, normal)
@@ -193,30 +261,29 @@ end
 net.Receive("Deformation_Apply", function()
     local hitent = net.ReadEntity()
     local hitPos = net.ReadVector()
-    local hitNormal = net.ReadNormal()
+
     local net_ReadFloat = net.ReadFloat
+
     local strength = net_ReadFloat()
     local radius = net_ReadFloat()
+    radius = radius * radius
 
-    local worldlocal = hitent:WorldToLocal(hitPos)
+    local localpos = hitent:WorldToLocal(hitPos)
+    local shitpos = GetNormalized(-localpos) * strength
+    local vertices = hitent.DeformedVertc
 
-    local DEFORMED_VERTICES = hitent.DeformedVertc
+    for i = 1, #vertices do
+        local vertex = vertices[i]
+        local pos = vertex.pos
+        local distance = DistToSqr(pos, localpos)
 
-    for I=1, #DEFORMED_VERTICES do
-        local vertex = DEFORMED_VERTICES[I]
-        local vertpos = vertex.pos
-        local dist = vertpos:Distance(worldlocal)
-        if dist < radius then
-            local direction = (vertpos + worldlocal):GetNormalized() + hitNormal
-            local distance3 = math_Clamp(1 - (dist / radius), 0, 1)
-
-            local direction2 = direction * strength * distance3
-
-            vertex.pos = vertpos - direction2
+        if distance < radius then
+            local distance2 = 1 - (distance / radius)
+            vertex.pos = pos + shitpos * distance2
         end
     end
 
-    hitent:GenerateMesh(DEFORMED_VERTICES)
+    hitent:GenerateMesh(vertices)
 end)
 
 function ENT:GenerateMesh(mesh)
@@ -228,40 +295,43 @@ function ENT:GenerateMesh(mesh)
 end
 
 local mat = Material("models/shiny")
+
 function ENT:GetRenderMesh()
     if !self.RENDER_MESH then return end
     return { Mesh = self.RENDER_MESH, Material = self.MAT or mat }
 end
 
 function ENT:SubdivideMesh(vertices)
-    local newVertices = {}
+    local shitverts = {}
 
     local function SUB(v1, v2)
-        return {
-            pos = (v1.pos + v2.pos) / 2
-        }
+        return { pos = (v1 + v2) / 2 }
     end
 
     for i = 1, #vertices, 3 do
         local v1, v2, v3 = vertices[i], vertices[i+1], vertices[i+2]
 
-        local subAB = SUB(v1, v2)
-        local subBC = SUB(v2, v3)
-        local subCA = SUB(v3, v1)
+        local vpos = v1.pos
+        local vpos2 = v2.pos
+        local vpos3 = v3.pos
 
-        table_insert(newVertices, subAB)
-        table_insert(newVertices, subBC)
-        table_insert(newVertices, subCA)
-        table_insert(newVertices, v1)
-        table_insert(newVertices, subAB)
-        table_insert(newVertices, subCA)
-        table_insert(newVertices, subAB)
-        table_insert(newVertices, v2)
-        table_insert(newVertices, subBC)
-        table_insert(newVertices, subCA)
-        table_insert(newVertices, subBC)
-        table_insert(newVertices, v3)
+        local subAB = SUB(vpos, vpos2)
+        local subBC = SUB(vpos2, vpos3)
+        local subCA = SUB(vpos3, vpos)
+
+        shitverts[#shitverts + 1] = subAB
+        shitverts[#shitverts + 1] = subBC
+        shitverts[#shitverts + 1] = subCA
+        shitverts[#shitverts + 1] = v1
+        shitverts[#shitverts + 1] = subAB
+        shitverts[#shitverts + 1] = subCA
+        shitverts[#shitverts + 1] = subAB
+        shitverts[#shitverts + 1] = v2
+        shitverts[#shitverts + 1] = subBC
+        shitverts[#shitverts + 1] = subCA
+        shitverts[#shitverts + 1] = subBC
+        shitverts[#shitverts + 1] = v3
     end
 
-    return newVertices
+    return shitverts
 end
